41acddd6032bacc79268edfa32d8f3a2be194532
Valentin <valentin.geras@yandex.ru>
2024-10-23 13:51:42 +0300

    тест

--- src/main/java/com/example/demo/controller/DollController.java
+++ src/main/java/com/example/demo/controller/DollController.java
@@ -1,15 +1,18 @@
 package com.example.demo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Slf4j
 @RequestMapping("dolls")
 public class DollController {
 
     @GetMapping
     public String getDolls() {
+        log.info("Response send: status 200");
         return "hello";
     }
 }
